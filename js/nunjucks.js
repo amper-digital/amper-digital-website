c23e9efad2361ebbd59c7acb7615bbeea68b598c
Tara Rhoseyn <taragulwell@gmail.com>
2023-10-08 16:21:30 +0100

    refactor: objectify nunjucks logic

--- js/nunjucks.js
+++ js/nunjucks.js
@@ -3,37 +3,42 @@ const fs = require('fs');
 
 nunjucks.configure('./templates', { autoescape: true });
 
-function createPage(filename, pageTitle) {
+class Page {
+    constructor(filename, pageTitle) {
+        this.filename = filename;
+        this.pageTitle = pageTitle;
+    }
 
-    // adapts file if it's the index.html (needs unique file paths and output location)
-    rel_file_path = '';
-    output_location = '';
-    if (filename !== 'index' && filename !== '404') { 
-        rel_file_path = '../' 
-        output_location = './pages/'
-    };
+    create() {
+        // adapts file if it's the index.html (needs unique file paths and output location)
+        const relFilePath = this.filename !== 'index' && this.filename !== '404' ? '../' : '';
+        const outputLocation = this.filename !== 'index' && this.filename !== '404' ? './pages/' : '';
 
-    fs.writeFileSync(
-        output_location+filename+'.html', nunjucks.render(filename+'.njk', { 
-            pageTitle: pageTitle+' | Amper Digital',
-            filePath: rel_file_path
-        }));
-};
+        fs.writeFileSync(
+            `${outputLocation}${this.filename}.html`,
+            nunjucks.render(`${this.filename}.njk`, {
+                pageTitle: `${this.pageTitle} | Amper Digital`,
+                filePath: relFilePath,
+            })
+        );
+    }
+}
+
+const pages = [
+    new Page('index', 'Home'),
+    new Page('404', '404 error'),
+    new Page('training', 'Training'),
+    new Page('testing', 'Testing'),
+    new Page('consulting', 'Consulting Services'),
+    new Page('accessibility-statement', 'Accessibility Statement'),
+    new Page('privacy-policy', 'Privacy Policy'),
+];
 
 try {
-    /*
-    createPage 1st param: name of .njk template, will also be set as name for .html file
-    createPage 2nd param: sets <title> attribute of html file
-    */
-    createPage('index', 'Home')
-    createPage('404', '404 error');
-    createPage('training', 'Training');
-    createPage('testing', 'Testing');
-    createPage('consulting', 'Consulting Services');
-    createPage('accessibility-statement', 'Accessibility Statement');
-    createPage('privacy-policy', 'Privacy Policy');
-    // success message:
+    for (const page of pages) {
+        page.create();
+    }
     console.log('Pages successfully created.');
-} catch(error) {
+} catch (error) {
     console.error('Error rendering or writing pages:', error);
-};
+}
